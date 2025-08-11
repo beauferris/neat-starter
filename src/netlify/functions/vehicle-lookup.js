c2597192063b962a58629270a808b79182ded97c
beauferris <h-beauferris@hotmail.com>
2025-08-10 22:54:03 -0600

    changed data model

--- src/netlify/functions/vehicle-lookup.js
+++
@@ -1,6 +0,0 @@
-// netlify/functions/vehicle-lookup.js
-exports.handler = async () => ({
-  statusCode: 200,
-  headers: { "content-type": "application/json" },
-  body: JSON.stringify({ ok: true }),
-});
